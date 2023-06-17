fa36708e2f9d4c3e8279e3fe2fb97661a1ab1937
Daniil848 <ladygindaniil848@gmail.com>
2023-06-17 12:12:51 +0300

    install tailwind css

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+const Header : FC = () => {
+
+  return (
+    <>
+      <header>
+        <nav>
+          <p>Новинки</p>
+          <p>Мужское</p>
+          <p>Женское</p>
+          <p>Аксесуары</p>
+        </nav>
+      </header>
+    </>
+  )
+};
+
+export default Header;
