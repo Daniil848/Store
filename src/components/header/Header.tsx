d6ee62c497f5fd4dedc966fece59032ceac7e139
Daniil848 <ladygindaniil848@gmail.com>
2023-06-17 13:38:53 +0300

    header WIP

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
+import styles from "./Header.module.scss";
 
 const Header : FC = () => {
 
   return (
     <>
-      <header>
-        <nav>
-          <p>Новинки</p>
-          <p>Мужское</p>
-          <p>Женское</p>
-          <p>Аксесуары</p>
+      <header className={styles.parent}>
+        <div className={styles.logo}></div>
+        <nav className={styles.nav}>
+          <p className={styles.navItem}>Новинки</p>
+          <p className={styles.navItem}>Мужское</p>
+          <p className={styles.navItem}>Женское</p>
+          <p className={styles.navItem}>Аксесуары</p>
         </nav>
+
       </header>
     </>
   )
