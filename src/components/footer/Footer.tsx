f754625cdbae734eac742831e6f937faf03ad035
Daniil848 <ladygindaniil848@gmail.com>
2023-11-03 19:52:05 +0300

    footer styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,26 @@
 import { FC } from "react";
+import styles from "../footer/Footer.module.scss"
 
 const Footer : FC = () => {
   return (
-    <>
-
-    </>
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.content1}></div>
+          <div className={styles.content2}></div>
+          <div className={styles.content3}></div>
+          <div className={styles.content4}></div>
+          <div className={styles.content4}></div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.content1}></div>
+          <div className={styles.content2}></div>
+          <div className={styles.content3}></div>
+          <div className={styles.content4}></div>
+          <div className={styles.content4}></div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
