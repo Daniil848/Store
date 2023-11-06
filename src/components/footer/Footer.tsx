f13cf80f8b0886baca7969a55b08ff4e2d86e4ba
Daniil848 <ladygindaniil848@gmail.com>
2023-11-06 15:09:24 +0300

    create db.json

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,24 +3,26 @@ import styles from "../footer/Footer.module.scss"
 
 const Footer : FC = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <div className={styles.content1}></div>
-          <div className={styles.content2}></div>
-          <div className={styles.content3}></div>
-          <div className={styles.content4}></div>
-          <div className={styles.content4}></div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.content1}></div>
-          <div className={styles.content2}></div>
-          <div className={styles.content3}></div>
-          <div className={styles.content4}></div>
-          <div className={styles.content4}></div>
-        </div>
-      </div>
-    </footer>
+    <>
+    </>
+    // <footer className={styles.footer}>
+    //   <div className={styles.wrapper}>
+    //     <div className={styles.content}>
+    //       <div className={styles.content1}></div>
+    //       <div className={styles.content2}></div>
+    //       <div className={styles.content3}></div>
+    //       <div className={styles.content4}></div>
+    //       <div className={styles.content4}></div>
+    //     </div>
+    //     <div className={styles.content}>
+    //       <div className={styles.content1}></div>
+    //       <div className={styles.content2}></div>
+    //       <div className={styles.content3}></div>
+    //       <div className={styles.content4}></div>
+    //       <div className={styles.content4}></div>
+    //     </div>
+    //   </div>
+    // </footer>
   );
 };
 
